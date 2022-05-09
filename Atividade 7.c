67cce9f9293e8dd708c844a7604469b4da3252de
Kauã Pereira <kaua1806@hotmail.com>
2022-05-09 16:52:25 -0300

    novotec

---
+++ Atividade 7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+	float s=0;
+	float a=0;
+	float k=0;
+	
+	printf("digite o valor do salário ");
+	scanf("%f",&s);
+	printf("digite o valor que deseja acrescentar ");
+	scanf("%f",&a);
+	k=s+a;
+	printf("%f",k);
+	k= (()a%100)*s)+s;
+	printf("valor do salario")
+}
